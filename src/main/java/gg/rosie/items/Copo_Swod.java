8a02ce1f042dcfb1eb7b22eba2da90218e3512d0
Rosie Hart <playinggameswithfriends@gmail.com>
2024-03-06 02:01:38 -0700

    separate material into its own file

--- src/main/java/gg/rosie/items/Copo_Swod.java
+++ src/main/java/gg/rosie/items/Copo_Swod.java
@@ -1,44 +1,7 @@
 package gg.rosie.items;
 
-import net.minecraft.item.Item;
-import net.minecraft.item.Items;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
-import net.minecraft.recipe.Ingredient;
-
-public class Copo_Material implements ToolMaterial {
-	@Override
-	public int getDurability() {
-		return 175;
-	}
-
-	@Override
-	public float getMiningSpeedMultiplier() {
-		return 5.0f;
-	}
-
-	@Override
-	public float getAttackDamage() {
-		return 5.0f;
-	}
-
-	@Override
-	public int getMiningLevel() {
-		return 2;
-	}
-
-	@Override
-	public int getEnchantability() {
-		return 15;
-	}
-
-	@Override
-	public Ingredient getRepairIngredient() {
-		return Ingredient.ofItems(Items.COPPER_INGOT);
-	}
-
-	public static final Copo_Material INSTANCE = new Copo_Material();
-}
 
 public class Copo_Swod extends SwordItem {
 	public Copo_Swod(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
---
+++ src/main/java/gg/rosie/materials/Copo_Material.java
@@ -0,0 +1,39 @@
+package gg.rosie.materials;
+
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+
+public class Copo_Material implements ToolMaterial {
+	@Override
+	public int getDurability() {
+		return 175;
+	}
+
+	@Override
+	public float getMiningSpeedMultiplier() {
+		return 5.0f;
+	}
+
+	@Override
+	public float getAttackDamage() {
+		return 5.0f;
+	}
+
+	@Override
+	public int getMiningLevel() {
+		return 2;
+	}
+
+	@Override
+	public int getEnchantability() {
+		return 15;
+	}
+
+	@Override
+	public Ingredient getRepairIngredient() {
+		return Ingredient.ofItems(Items.COPPER_INGOT);
+	}
+
+	public static final Copo_Material INSTANCE = new Copo_Material();
+}
