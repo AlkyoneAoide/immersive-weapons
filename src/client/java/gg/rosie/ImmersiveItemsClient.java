d2fb91148b690af278aa477a38b5190235f06be4
Rosie Hart <playinggameswithfriends@gmail.com>
2024-03-05 22:43:40 -0700

    properly use fabric template

---
+++ src/client/java/gg/rosie/ImmersiveItemsClient.java
@@ -0,0 +1,10 @@
+package gg.rosie;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ImmersiveItemsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
