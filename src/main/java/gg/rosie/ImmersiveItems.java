0aecbe1ad68a7541aa0dc2540a4fe6e8450c53c2
Sammy Hofmann <shammrocks.yt@gmail.com>
2024-03-06 00:03:08 -0700

    Added copo sowd (almost)

--- src/main/java/gg/rosie/ImmersiveItems.java
+++ src/main/java/gg/rosie/ImmersiveItems.java
@@ -2,6 +2,11 @@ package gg.rosie;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +16,12 @@ public class ImmersiveItems implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("immersive-items");
 
+	//Creating a new item
+	//Registering the item when the game is loaded
+	public static final Item COPO_SWOD =
+			Registry.register(Registries.ITEM, new Identifier("immersive-items", "copo_swod"),
+			new Item(new FabricItemSettings()));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -18,5 +29,7 @@ public class ImmersiveItems implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+
+		//Registering the item when the game is loaded
 	}
 }
