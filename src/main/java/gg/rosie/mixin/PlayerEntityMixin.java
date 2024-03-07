c18fc452f1bbc83c5609ddfd2b3d35a102f18896
Rosie Hart <playinggameswithfriends@gmail.com>
2024-03-07 02:10:45 -0700

    try add mixin stuff

---
+++ src/main/java/gg/rosie/mixin/PlayerEntityMixin.java
@@ -0,0 +1,15 @@
+package gg.rosie.mixin;
+
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin {
+	@ModifyVariable(method = "attack(Lnet/minecraft/entity/Entity;)V", at = @At("STORE"), ordinal = 2)
+	public boolean attack(boolean value) {
+		System.out.println("Crit prevented(?)");
+		return false;
+	}
+}
