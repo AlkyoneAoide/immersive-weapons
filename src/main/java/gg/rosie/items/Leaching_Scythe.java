e78a7670afe74fc4db972245464dd3eac1f77747
Rosie Hart <playinggameswithfriends@gmail.com>
2024-03-18 02:32:42 -0600

    null safe refactor

--- src/main/java/gg/rosie/items/Leaching_Scythe.java
+++ src/main/java/gg/rosie/items/Leaching_Scythe.java
@@ -1,14 +1,10 @@
 package gg.rosie.items;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LightningEntity;
+import gg.rosie.DamageHelper;
+
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.HoeItem;
 import net.minecraft.item.ToolMaterial;
-import net.minecraft.world.World;
-
-import gg.rosie.CustomItemCrits;
 
 import java.util.Random;
 
@@ -17,7 +13,11 @@ public class Leaching_Scythe extends HoeItem {
     private static final Random RANDOM = new Random();
     public Leaching_Scythe(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
         super(material, attackDamage, attackSpeed, settings);
-        CustomItemCrits.add("immersive-weapons:leaching_scythe", (source, amount) -> {
+        DamageHelper.ItemCrits.add("immersive-weapons:leaching_scythe", (source, amount) -> {
+            if (source.getAttacker() == null) {
+                return;
+            }
+
             if (source.getAttacker().getWorld().isNight()) {
                 if (RANDOM.nextInt(10) < 6) {
                     LivingEntity user = (LivingEntity) source.getAttacker();
