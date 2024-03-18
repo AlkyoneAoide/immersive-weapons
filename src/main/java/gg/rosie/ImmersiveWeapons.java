45e9921aa8c94ae388e1f472267679f8052c848e
Sammy Hofmann <shammrocks.yt@gmail.com>
2024-03-17 18:09:32 -0600

    Added Leaching scythe into game (texture and base functionality and crafting)

--- src/main/java/gg/rosie/ImmersiveWeapons.java
+++ src/main/java/gg/rosie/ImmersiveWeapons.java
@@ -2,6 +2,8 @@ package gg.rosie;
 
 import gg.rosie.materials.Copper_Material;
 import gg.rosie.items.Copper_Battleaxe;
+import gg.rosie.materials.Nether_Star_Material;
+import gg.rosie.items.Leaching_Scythe;
 
 import net.fabricmc.api.ModInitializer;
 
@@ -16,11 +18,16 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ImmersiveWeapons implements ModInitializer {
-	// Creating a new item
-	// Registering the item when the game is loaded
+	// Creating new items
+	// Registering the items when the game is loaded
 	public static final ToolItem COPPER_BATTLEAXE = Registry.register(Registries.ITEM,
 			new Identifier("immersive-weapons", "copper_battleaxe"),
 			new Copper_Battleaxe(Copper_Material.INSTANCE, 2, -3.2f, new FabricItemSettings().maxCount(1)));
+
+	public static final ToolItem LEACHING_SCYTHE = Registry.register(Registries.ITEM,
+			new Identifier("immersive-weapons", "leaching_scythe"),
+			new Leaching_Scythe(Nether_Star_Material.INSTANCE, 2, -3.2f, new FabricItemSettings().maxCount(1)));
+
 
 	// Create immersiveWeapons inventory tab
 	private static final ItemGroup WEAPON_GROUP = FabricItemGroup.builder()
@@ -28,6 +35,7 @@ public class ImmersiveWeapons implements ModInitializer {
 			.displayName(Text.translatable("itemGroup.immersive-weapons.weapons"))
 			.entries((context, entries) -> {
 				entries.add(COPPER_BATTLEAXE);
+				entries.add(LEACHING_SCYTHE);
 			})
 			.build();
 
