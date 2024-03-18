5947f7d7d05576300e26aaa6a3ef51fd886f7ae1
Sammy Hofmann <shammrocks.yt@gmail.com>
2024-03-17 23:15:49 -0600

    Added Leaching scythe crit healing

--- src/main/java/gg/rosie/items/Leaching_Scythe.java
+++ src/main/java/gg/rosie/items/Leaching_Scythe.java
@@ -2,15 +2,29 @@ package gg.rosie.items;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LightningEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.HoeItem;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.world.World;
 
 import gg.rosie.CustomItemCrits;
 
+import java.util.Random;
+
 public class Leaching_Scythe extends HoeItem {
+
+    private static final Random RANDOM = new Random();
     public Leaching_Scythe(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
         super(material, attackDamage, attackSpeed, settings);
-        //CustomItemCrits.add("immersive-weapons:copper_battleaxe", (source, amount) -> {});
+        CustomItemCrits.add("immersive-weapons:leaching_scythe", (source, amount) -> {
+            if (source.getAttacker().getWorld().isNight()) {
+                if (RANDOM.nextInt(10) < 6) {
+                    LivingEntity user = (LivingEntity) source.getAttacker();
+
+                    user.setHealth((user.getHealth() + amount/4));
+                }
+            }
+        });
     }
 }
