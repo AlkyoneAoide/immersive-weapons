cd0860e0b0f5d83414061b41a4919ac17ed00cab
Rosie Hart <playinggameswithfriends@gmail.com>
2024-03-17 23:33:23 -0600

    Prototype for copper battleaxe lightning immunity

--- src/main/java/gg/rosie/items/Copper_Battleaxe.java
+++ src/main/java/gg/rosie/items/Copper_Battleaxe.java
@@ -5,7 +5,9 @@ import java.util.Random;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LightningEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.AxeItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.world.World;
 
@@ -13,6 +15,8 @@ import gg.rosie.CustomItemCrits;
 
 public class Copper_Battleaxe extends AxeItem {
 	private static final Random RANDOM = new Random();
+
+	private boolean isSelected = false;
 
 	public Copper_Battleaxe(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
 		super(material, attackDamage, attackSpeed, settings);
@@ -32,5 +36,37 @@ public class Copper_Battleaxe extends AxeItem {
 				}
 			}
 		});
+	}
+
+	private void updateSelectionStatus(boolean flag) {
+		if (isSelected == flag) {
+			return;
+		}
+
+		if (flag) {
+			// DamageHelper.Immunity.add(id, minecraft:lightning_bolt);
+		} else {
+			// DamageHelper.Immunity.remove(id, minecraft:lightning_bolt);
+		}
+
+		isSelected = flag;
+	}
+
+	@Override
+	public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+		if (!world.isClient) {
+			if (entity instanceof PlayerEntity) {
+				PlayerEntity player = (PlayerEntity) entity;
+
+				for (ItemStack handItem : player.getHandItems()) {
+					if (handItem.equals(stack)) {
+						updateSelectionStatus(true);
+						return;
+					}
+				}
+
+				updateSelectionStatus(false);
+			}
+		}
 	}
 }
