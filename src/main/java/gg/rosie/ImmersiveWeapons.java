db8f46480df38d2da9a877a734973acdd9364231
Rosie Hart <playinggameswithfriends@gmail.com>
2024-04-02 11:01:02 -0600

    add base implementation of soul vitric

--- src/main/java/gg/rosie/ImmersiveWeapons.java
+++ src/main/java/gg/rosie/ImmersiveWeapons.java
@@ -1,5 +1,6 @@
 package gg.rosie;
 
+import gg.rosie.blocks.Soul_Vitric;
 import gg.rosie.items.Soul_Claymore;
 import gg.rosie.materials.Copper_Material;
 import gg.rosie.items.Copper_Battleaxe;
@@ -11,6 +12,9 @@ import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.*;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolItem;
@@ -20,19 +24,30 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ImmersiveWeapons implements ModInitializer {
+	public static final String NAMESPACE = "immersive-weapons";
+
 	// Creating new items
 	// Registering the items when the game is loaded
 	public static final ToolItem COPPER_BATTLEAXE = Registry.register(Registries.ITEM,
-			new Identifier("immersive-weapons", "copper_battleaxe"),
+			new Identifier(NAMESPACE, "copper_battleaxe"),
 			new Copper_Battleaxe(Copper_Material.INSTANCE, 2, -3.2f, new FabricItemSettings().maxCount(1)));
 
 	public static final ToolItem LEACHING_SCYTHE = Registry.register(Registries.ITEM,
-			new Identifier("immersive-weapons", "leaching_scythe"),
+			new Identifier(NAMESPACE, "leaching_scythe"),
 			new Leaching_Scythe(Nether_Star_Material.INSTANCE, 0, -1.0f, new FabricItemSettings().maxCount(1)));
 
 	public static final ToolItem SOUL_CLAYMORE = Registry.register(Registries.ITEM,
-			new Identifier("immersive-weapons", "soul_claymore"),
+			new Identifier(NAMESPACE, "soul_claymore"),
 			new Soul_Claymore(Soul_Glass_Material.INSTANCE, 0, -1.0f, new FabricItemSettings().maxCount(1)));
+
+	// Creating and registering new blocks + their corresponding items
+	public static final Block SOUL_VITRIC = Registry.register(Registries.BLOCK,
+			new Identifier(NAMESPACE, "soul_vitric"),
+			new Soul_Vitric(FabricBlockSettings.copyOf(Blocks.GLASS).strength(3.0f).nonOpaque())); // change strength to match glass??? or be similar...
+
+	public static final BlockItem SOUL_VITRIC_ITEM = Registry.register(Registries.ITEM,
+			new Identifier(NAMESPACE, "soul_vitric"),
+			new BlockItem(SOUL_VITRIC, new FabricItemSettings()));
 
 
 	// Create immersiveWeapons inventory tab
@@ -43,6 +58,8 @@ public class ImmersiveWeapons implements ModInitializer {
 				entries.add(COPPER_BATTLEAXE);
 				entries.add(LEACHING_SCYTHE);
 				entries.add(SOUL_CLAYMORE);
+
+				entries.add(SOUL_VITRIC_ITEM);
 			})
 			.build();
 
---
+++ src/main/java/gg/rosie/blocks/Soul_Vitric.java
@@ -0,0 +1,10 @@
+package gg.rosie.blocks;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+
+public class Soul_Vitric extends Block {
+    public Soul_Vitric(Settings settings) {
+        super(settings);
+    }
+}
