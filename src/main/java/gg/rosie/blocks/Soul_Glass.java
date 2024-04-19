b1901c348710a37dd17d1c75cbdb43368234c9e4
Rosie Hart <playinggameswithfriends@gmail.com>
2024-04-19 09:27:31 -0600

    add soul glass as a beacon light provider

--- src/main/java/gg/rosie/blocks/Soul_Glass.java
+++ src/main/java/gg/rosie/blocks/Soul_Glass.java
@@ -1,9 +1,16 @@
 package gg.rosie.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockRenderType;
 
-public class Soul_Glass extends Block {
+public class Soul_Glass extends Block implements IBeaconBeamColorProvider {
 	public Soul_Glass(Settings settings) {
 		super(settings);
+	}
+
+	@Override
+	public BlockRenderType getRenderType(BlockState state) {
+		return BlockRenderType.MODEL;
 	}
 }
