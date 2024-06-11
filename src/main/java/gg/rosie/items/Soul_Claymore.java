670e3e97bae2320b1b0e05a48da6329a92067c77
Shammrocks <shammrocks.yt@gmail.com>
2024-06-10 18:52:18 -0600

    Working on soul claymore sound effects

--- src/main/java/gg/rosie/items/Soul_Claymore.java
+++ src/main/java/gg/rosie/items/Soul_Claymore.java
@@ -16,6 +16,7 @@ import java.util.Random;
 
 public class Soul_Claymore extends SwordItem {
 
+    private float previousDamagePercent;
     private static final Random RANDOM = new Random();
     public Soul_Claymore(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
         super(material, attackDamage, attackSpeed, settings);
@@ -25,20 +26,26 @@ public class Soul_Claymore extends SwordItem {
     @Override
     public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         boolean result = super.postHit(stack, target, attacker);
-        playBreakSound(stack.getDamage() / stack.getMaxDamage(), attacker);
+        playBreakSound(stack.getDamage(), stack.getMaxDamage(), attacker);
         return result;
     }
 
     @Override
     public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
         boolean result = super.postMine(stack, world, state, pos, miner);
-        playBreakSound(stack.getDamage() / stack.getMaxDamage(), miner);
+        playBreakSound(stack.getDamage(), stack.getMaxDamage(), miner);
         return result;
     }
 
-    private void playBreakSound(int damagePercent, LivingEntity player) {
-        if(damagePercent == 25 || damagePercent == 50 || damagePercent == 75)
+    private void playBreakSound(float damage, int maxDamage, LivingEntity player) {
+        float damagePercent = (((maxDamage - damage) / maxDamage) * 100);
+        if(damagePercent % 25 == 0 ||
+                (previousDamagePercent > 75 && damagePercent < 75) ||
+                (previousDamagePercent > 50 && damagePercent < 50) ||
+                (previousDamagePercent > 25 && damagePercent < 25))
             if(!player.getWorld().isClient)
                 player.getWorld().playSound(null, player.getBlockPos(), SoundEvents.BLOCK_GLASS_BREAK, SoundCategory.BLOCKS, 1, 1);
+
+        previousDamagePercent = damagePercent;
     }
 }
